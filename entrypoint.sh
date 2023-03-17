ea88909b8e32d605a6dcad73c83eee0f4bef8ad0
KjellGeerts <kjell.geerts@biztory.be>
2023-03-17 12:35:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+cd workbooks/
 tabcmd login --server https://dub01.online.tableau.com --site biztorybenelux --token-name publish_token --token-value ptUMOuxeQmigIPIH0hkW4A==:Fw2QQXtcuyFmsOVnjnqGBA0gOWhDNPJ2
 tabcmd publish "yelp_analyses.twb" -n "Yelp Analyses" -r "Sandbox" --db-username "TABLEAU_USER" --db-password "Kjell.01" --save-db-password -o
 tabcmd logout
