eb1bbc9b63bca531d596e4e8aed3c8baac3d15ae
Kjell Geerts <kjell.geerts@biztory.be>
2023-07-20 10:43:36 +0200

    new changes'

--- tableau_api.py
+++ tableau_api.py
@@ -1,162 +1,122 @@
-# -*- coding: utf-8 -*-
-"""
-@author: jayaharyonomanik
-"""
-
-
-import logging
-import requests
-import xmltodict
 import tableauserverclient as TSC
-import xml.dom.minidom as minidom
-from collections import defaultdict
-
-import util
-
-API_VERSION = '3.9'
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-logging.basicConfig(format='%(asctime)s %(message)s')
-
+from dotenv import load_dotenv
+import git
+import os
+import shutil
+from zipfile import ZipFile
+import yaml
+from pathlib import Path
+import threading
 
 class TableauApi:
-    def __init__(self, username, password, tableau_api_url, tableau_url, site_id):
-        self.username = username
-        self.password = password
-        self.tableau_api_url = tableau_api_url
-        self.tableau_url = tableau_url
-        self.site_id = site_id
+    load_dotenv()
+    pat_name = os.getenv('TABLEAU_PAT_NAME')
+    pat_pw = os.getenv('TABLEAU_PAT_PW')
+    tableau_url = os.getenv('TABLEAU_SERVER')
+    site_id = os.getenv('TABLEAU_SITE')
+    connection_user = os.getenv('CONNECTION_USERNAME')
+    connection_pw = os.getenv('CONNECTION_PW')
 
-
-    def sign_in(self):
-        payload = \
-        f"""<tsRequest>
-          <credentials name="{ self.username }" password="{ self.password }" >
-            <site contentUrl="" />
-          </credentials>
-        </tsRequest>"""
-        response = requests.post(f'{self.tableau_api_url}{API_VERSION}/auth/signin', data=payload)
-        doc = minidom.parseString(response.text)
-        return doc.getElementsByTagName('credentials')[0].getAttribute("token")
-
-
-    def get_all_projects(self):
-        token = self.sign_in()
-        headers = {
-            'X-Tableau-Auth': token
-        }
-        response = requests.get(f'{self.tableau_api_url}{API_VERSION}/sites/{self.site_id}/projects?pageSize=1000', headers=headers)
-        all_projects_response = xmltodict.parse(response.text)
-        try:
-            all_projects_response = all_projects_response['tsResponse']
-            all_projects = all_projects_response['projects']['project']
-            return all_projects
-        except Exception as e:
-            logging.error("Error parsing project response")
-            logging.error(e.message)
-            return None
-
-
-    def list_all_data_sources(self):
-        tableau_auth = TSC.TableauAuth(self.username, self.password)
-        server = TSC.Server(self.tableau_url)
-
-        with server.auth.sign_in(tableau_auth):
-            all_datasources, pagination_item = server.datasources.get()
-            logging.info(f"There are { pagination_item.total_available } datasources on site: ")
-
-            while len(all_datasources) < pagination_item.total_available:
-                request_options = TSC.RequestOptions(pagenumber=pagination_item.page_number+1)
-                datasources, pagination_item = server.datasources.get(request_options)
-                all_datasources.extend(datasources)
-            return all_datasources
-
-
-    def list_all_workbooks(self):
-        tableau_auth = TSC.TableauAuth(self.username, self.password)
-        server = TSC.Server(self.tableau_url)
-
-        with server.auth.sign_in(tableau_auth):
-            all_workbooks, pagination_item = server.workbooks.get()
-            logging.info(f"There are { pagination_item.total_available } workbooks on site: ")
-
-            while len(all_workbooks) < pagination_item.total_available:
-                request_options = TSC.RequestOptions(pagenumber=pagination_item.page_number+1)
-                workbooks, pagination_item = server.workbooks.get(request_options)
-                all_workbooks.extend(workbooks)
-            return all_workbooks
-
-
-    def get_workbook_detail(self, workbook_id):
-        tableau_auth = TSC.TableauAuth(self.username, self.password)
-        server = TSC.Server(self.tableau_url)
-
-        with server.auth.sign_in(tableau_auth):
-            workbook = server.workbooks.get_by_id(workbook_id)
-            return workbook
-
-
-    def delete_workbook(self, workbook_id):
-        tableau_auth = TSC.TableauAuth(self.username, self.password)
-        server = TSC.Server(self.tableau_url)
-
-        with server.auth.sign_in(tableau_auth):
-            response = server.workbooks.delete(workbook_id)
-            return response
-
-
-    def get_project_id_by_path_with_tree(self, project_path):
-        project_name = project_path.split("/")[-1]
-
-        all_projects = self.get_all_projects()
-        project_tree =  util.parse_projects_to_tree(all_projects)
-        project_candidate = util.find_project_by_name(project_name, all_projects)
-
-        project_path_dict = defaultdict(lambda: None)
-        for project in project_candidate:
-            nodes = list(project_tree.rsearch(project['@id']))
-            nodes.reverse()
-            node_path = "/".join([project_tree.get_node(node).data for node in nodes[1:]])
-            project_path_dict[node_path] = project['@id']
-
-        return project_path_dict[project_path]
-
-
-    def create_project_by_path(self, project_path):
-        project_path_split = project_path.split("/")
-
-        last_project_id = None
-        max_index = 0
-        while self.get_project_id_by_path_with_tree("/".join(project_path_split[:max_index+1])) is not None:
-            last_project_id = self.get_project_id_by_path_with_tree("/".join(project_path_split[:max_index+1]))
-            max_index = max_index + 1
-
-        for i in range(max_index, len(project_path_split)):
-            logging.info("Creating Project within path : { '/'.join(project_path_split[:i+1]) }")
-            new_project = TSC.ProjectItem(project_path_split[i],
-                                          description=None,
-                                          content_permissions=TSC.ProjectItem.ContentPermissions.ManagedByOwner,
-                                          parent_id=last_project_id)
-
-            tableau_auth = TSC.TableauAuth(self.username, self.password)
-            server = TSC.Server(self.tableau_url)
-            with server.auth.sign_in(tableau_auth):
-                new_project = server.projects.create(new_project)
-                last_project_id = new_project.id
-        return last_project_id
-
-
-    # Still figuring out how to put description in workbook via this api
-    def publish_workbook(self, name, project_id, file_path, hidden_views = None, show_tabs = False, tags = None, description = None):
-        tableau_auth = TSC.TableauAuth(self.username, self.password)
-        server = TSC.Server(self.tableau_url)
+    def sign_in():
+        tableau_auth = TSC.PersonalAccessTokenAuth(os.getenv('TABLEAU_PAT_NAME'), os.getenv('TABLEAU_PAT_PW'), site_id= os.getenv('TABLEAU_SITE'))
+        server = TSC.Server(os.getenv('TABLEAU_SERVER'), use_server_version=True)
         server.auth.sign_in(tableau_auth)
-        new_workbook = TSC.WorkbookItem(name = name, project_id = project_id, show_tabs=show_tabs)
-        new_workbook = server.workbooks.publish(new_workbook, file_path, TSC.Server.PublishMode.Overwrite, hidden_views=hidden_views)
+        return server
 
-        if tags is not None:
-            new_workbook.tags = set(tags)
-            new_workbook = server.workbooks.update(new_workbook)
+    def set_connection_credentials(connection_name, connection_password, connection_embed, connection_oauth):
+        credentials = TSC.ConnectionCredentials(connection_name,connection_password,embed=connection_embed,oauth=connection_oauth)
+        return credentials
 
-        return new_workbook
+    def get_file_path(input_file_name):
+        for root, dirs, files in os.walk("."): 
+            for filename in files:
+                if filename == input_file_name:
+                    filepath = os.path.join(root, filename)
+                    return filepath
+
+    def get_tableau_server_folder_id(filtered_project):
+        selected_project = ''
+        # get all projects on site
+        all_project_items, pagination_item = TableauApi.sign_in().projects.get()
+        for project in all_project_items:
+                if project.name == filtered_project:
+                        selected_project = project.id
+        return selected_project
+
+    def get_current_branch():
+        repo = git.Repo(search_parent_directories=True)
+        branch = repo.active_branch
+        return branch.name
+
+    def create_domain_tableau_server_project(workbook_name, project_name):
+            if TableauApi.get_tableau_server_folder_id(project_name) == '':
+                domain_project = TSC.ProjectItem(name=project_name, content_permissions='LockedToProject')
+                TableauApi.sign_in().projects.create(domain_project)
+                certified_content = TSC.ProjectItem(name='Certified Content', content_permissions='LockedToProject', parent_id= TableauApi.get_tableau_server_folder_id(project_name))
+                TableauApi.sign_in().projects.create(certified_content)
+
+    def create_branch_tableau_server_project(parent_folder, branch_name):
+        if TableauApi.get_tableau_server_folder_id(TableauApi.get_current_branch()) == '':
+        # create project & project item
+            branch_project = TSC.ProjectItem(name=branch_name, content_permissions='LockedToProject', parent_id=parent_folder)
+            TableauApi.sign_in().projects.create(branch_project)
+
+    def publish_tableau_server_workbook(input_tableau_workbook, input_project_id, input_connection_credentials, workbook_file_name):
+        with open(TableauApi.get_file_path(workbook_file_name + '.twbx'), 'rb') as workbook_file:
+            workbook_item = TSC.WorkbookItem(name = input_tableau_workbook, project_id = input_project_id, show_tabs = True)
+            workbook_item = TableauApi.sign_in().workbooks.publish(workbook_item = workbook_item, file = workbook_file, mode = 'Overwrite', connection_credentials = input_connection_credentials)
+
+    def delete_tableau_server_project(input_project_id):
+        TableauApi.sign_in().projects.delete(input_project_id)
+
+    def copy_extract_tableau_workbook(original_workbook):
+        os.mkdir('./temp')
+        shutil.copyfile(TableauApi.get_file_path(original_workbook + '.twbx'), './temp/' + original_workbook + '.zip')
+        os.chdir('./temp/')
+        with ZipFile('accounting_workbook.zip', 'r') as f:
+            f.extractall()
+
+    def replace_connection_details(environment, original_workbook):
+        with open("../connection.yml", "r") as stream:
+            output = yaml.safe_load(stream)
+            with open(original_workbook + '.twb', 'r') as file :
+                filedata = file.read()
+                filedata = filedata.replace(output['connection']['server']['dev'], output['connection']['server'][environment])
+                filedata = filedata.replace(output['connection']['database']['dev'], output['connection']['database'][environment])
+                filedata = filedata.replace(output['connection']['schema']['dev'], output['connection']['schema'][environment])
+                filedata = filedata.replace(output['connection']['username']['dev'], output['connection']['username'][environment])
+                with open('./' + original_workbook + '_' + environment + '.twb', 'w') as file:
+                    file.write(filedata)
+
+    def create_twbx_file(twb_filename):
+        with ZipFile('./' + twb_filename + '.twbx', 'w') as twbx_zip:
+            twbx_zip.write('./' + twb_filename + '.twb', os.path.basename(twb_filename + '.twb'))
+
+    def remove_temp_folder():
+        os.chdir('../')
+        shutil.rmtree('./temp')
+
+def content_migration(workbook_name, environment):
+    with open("./content.yml", "r") as stream:
+        content = yaml.safe_load(stream)
+        project_name = content['content'][workbook_name]['project']
+        dashboard_title = content['content'][workbook_name]['title']
+        workbook_filename = content['content'][workbook_name]['filename'] + '_' + environment   
+
+        TableauApi.copy_extract_tableau_workbook(workbook_name)
+        TableauApi.replace_connection_details(environment, workbook_name)
+        TableauApi.create_twbx_file(workbook_name + '_' + environment)
+        
+        if environment == 'dev':
+            TableauApi.create_branch_tableau_server_project(TableauApi.get_tableau_server_folder_id('DEV'),TableauApi.get_current_branch())
+            TableauApi.publish_tableau_server_workbook(dashboard_title ,TableauApi.get_tableau_server_folder_id(TableauApi.get_current_branch()),TableauApi.set_connection_credentials(os.getenv('CONNECTION_USERNAME'),os.getenv('CONNECTION_PW'),True, False), workbook_filename)
+        else :
+            TableauApi.create_domain_tableau_server_project(workbook_name, project_name)
+            all_projects = list(TSC.Pager(TableauApi.sign_in().projects))
+            for project in all_projects:
+                if project.parent_id == TableauApi.get_tableau_server_folder_id(project_name):
+                    TableauApi.publish_tableau_server_workbook(dashboard_title ,project.id,TableauApi.set_connection_credentials(os.getenv('CONNECTION_USERNAME'),os.getenv('CONNECTION_PW'),True, False),workbook_filename)
+
+        TableauApi.remove_temp_folder()
+
+content_migration('accounting_workbook', '')
